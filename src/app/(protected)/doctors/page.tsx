d5a5b612da2c8b3a2220789e110b68077813eb51
saraivaa.me <wesley-saraiva@outlook.com>
2025-05-30 21:03:54 -0300

    FEAT: add page container components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,26 @@
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions></PageActions>
+      </PageHeader>
+      <PageContent></PageContent>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
